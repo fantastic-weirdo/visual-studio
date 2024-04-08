2b52d9348266beca4737e1d10f3876ed6fe4a17c
fantastic-weirdo <st2300200@gmail.com>
2024-04-08 14:25:54 +0800

    4/8實現基本四則運算

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -16,7 +16,9 @@ namespace Calculator
         {
             InitializeComponent();
         }
-
+        //全域變數
+        float firstNumber, secondNumber;
+        int operators = -1;//選擇哪種運算模式(加:0，減:1，乘:2，除:3，-1:重新設定
         private void txtNumber_TextChanged(object sender, EventArgs e)
         {
 
@@ -104,27 +106,60 @@ namespace Calculator
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
+            float finalResults = 0f; //宣告最後計算結果變數
+            secondNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
 
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
+
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
-
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 3; //選擇「除」號
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 2; //選擇「乘」號
         }
 
         private void btnMinus_Click(object sender, EventArgs e)
         {
-
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 1; //選擇「減」號
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 0;
         }
     }
 }
