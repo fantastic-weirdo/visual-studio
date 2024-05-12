6878b1fc76ad150e9c9650f262ab510ba110683c
fantastic-weirdo <st2300200@gmail.com>
2024-05-12 11:54:13 +0800

    5/12增加儲存功能

--- NotePad/Form1.cs
+++ NotePad/Form1.cs
@@ -82,7 +82,44 @@ namespace NotePad
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            // 設置對話方塊標題
+            saveFileDialog1.Title = "儲存檔案";
+            // 設置對話方塊篩選器，限制使用者只能儲存特定類型的檔案
+            saveFileDialog1.Filter = "文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*";
+            // 如果希望預設儲存的檔案類型是文字檔案，可以這樣設置
+            saveFileDialog1.FilterIndex = 1;
+            // 如果希望對話方塊在儲存時顯示的初始目錄，可以設置 InitialDirectory
+            saveFileDialog1.InitialDirectory = "C:\\";
 
+            // 顯示對話方塊，並等待使用者指定儲存檔案的路徑
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+            // 檢查使用者是否指定了儲存檔案的路徑
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    // 取得使用者指定的儲存檔案路徑
+                    string saveFilePath = saveFileDialog1.FileName;
+
+                    // 使用 StreamWriter 將 RichTextBox 中的內容寫入到檔案中
+                    using (StreamWriter streamWriter = new StreamWriter(saveFilePath))
+                    {
+                        streamWriter.Write(rtbText.Text);
+                    }
+
+                    MessageBox.Show("檔案已成功儲存。", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // 如果發生錯誤，用 MessageBox 顯示錯誤訊息
+                    MessageBox.Show("儲存檔案時發生錯誤: " + ex.Message, "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("使用者取消了儲存檔案操作。", "訊息", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
