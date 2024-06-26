ce1fcb127299737949092b6999ef219feb24d265
fantastic-weirdo <st2300200@gmail.com>
2024-06-27 02:10:51 +0800

    6/27
    確認五個鬧鐘都會響

--- clock/Form1.cs
+++ clock/Form1.cs
@@ -1,106 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using NAudio.Wave;           // 音效檔播放器函式庫
-using System.IO;             // 檔案讀取的IO函式庫
+using NAudio.Wave;
+using System.IO;
 
 namespace clock
 {
-    public partial class Form1 : System.Windows.Forms.Form
+    public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
             comboboxInitialzation();
-            timerClcok.Start();       // 啟動時鐘
+            timerClcok.Start();
         }
-        List<string> hours = new List<string>();            // 小時清單
-        List<string> minutes = new List<string>();          // 分鐘清單   
 
-        // 下拉選單初始化
+        List<string> hours = new List<string>();
+        List<string> minutes = new List<string>();
+
         private void comboboxInitialzation()
         {
-            // 設定小時下拉選單的選單內容，建立小時的清單，數字範圍為00-23
             for (int i = 0; i <= 23; i++)
+            {
                 cmbHour.Items.Add(string.Format("{0:00}", i));
-            cmbHour.SelectedIndex = 0;
-            for (int i = 0; i <= 23; i++)
                 cmbHour1.Items.Add(string.Format("{0:00}", i));
-            cmbHour1.SelectedIndex = 0;
-            for (int i = 0; i <= 23; i++)
                 cmbHour2.Items.Add(string.Format("{0:00}", i));
-            cmbHour2.SelectedIndex = 0;
-            for (int i = 0; i <= 23; i++)
                 cmbHour3.Items.Add(string.Format("{0:00}", i));
-            cmbHour3.SelectedIndex = 0;
-            for (int i = 0; i <= 23; i++)
                 cmbHour4.Items.Add(string.Format("{0:00}", i));
+            }
+            cmbHour.SelectedIndex = 0;
+            cmbHour1.SelectedIndex = 0;
+            cmbHour2.SelectedIndex = 0;
+            cmbHour3.SelectedIndex = 0;
             cmbHour4.SelectedIndex = 0;
-            // 設定分鐘下拉選單的選單內容，建立分鐘的清單，數字範圍為00-59
+
             for (int i = 0; i <= 59; i++)
+            {
                 cmbMin.Items.Add(string.Format("{0:00}", i));
-            cmbMin.SelectedIndex = 0;
-            for (int i = 0; i <= 59; i++)
                 cmbMin1.Items.Add(string.Format("{0:00}", i));
-            cmbMin1.SelectedIndex = 0;
-            for (int i = 0; i <= 59; i++)
                 cmbMin2.Items.Add(string.Format("{0:00}", i));
-            cmbMin2.SelectedIndex = 0;
-            for (int i = 0; i <= 59; i++)
                 cmbMin3.Items.Add(string.Format("{0:00}", i));
-            cmbMin3.SelectedIndex = 0;
-            for (int i = 0; i <= 59; i++)
                 cmbMin4.Items.Add(string.Format("{0:00}", i));
+            }
+            cmbMin.SelectedIndex = 0;
+            cmbMin1.SelectedIndex = 0;
+            cmbMin2.SelectedIndex = 0;
+            cmbMin3.SelectedIndex = 0;
             cmbMin4.SelectedIndex = 0;
         }
-        
+
         private void timerClcok_Tick(object sender, EventArgs e)
         {
-            txtClock.Text = DateTime.Now.ToString("HH:mm:ss");    // 顯示時間
+            txtClock.Text = DateTime.Now.ToString("HH:mm:ss");
         }
-        private string strSelectTime;                       // 鬧鐘時間設定
-        private WaveOutEvent waveOut;                       // 音效檔播放器
-        private AudioFileReader audioFileReader;            // 音效檔讀取器
+
+        private List<string> strSelectTimes = new List<string>();  // 鬧鐘時間設定
+        private List<bool> alarmsRung = new List<bool> { false, false, false, false, false }; // 記錄鬧鐘是否響過
+        private WaveOutEvent waveOut;
+        private AudioFileReader audioFileReader;
+
         private void timerAlert_Tick(object sender, EventArgs e)
         {
-            // 判斷現在時間是不是已經是鬧鐘設定時間？如果時間到了，就要播放鬧鐘聲音
-            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+            string currentTime = DateTime.Now.ToString("HH:mm");
+
+            for (int i = 0; i < strSelectTimes.Count; i++)
             {
-                try
+                if (strSelectTimes[i] == currentTime && !alarmsRung[i])
                 {
-                    stopWaveOut();
+                    try
+                    {
+                        stopWaveOut();
 
-                    // 指定聲音檔的相對路徑，可以使用MP3
-                    string audioFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "alert.wav");
+                        string audioFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "alert.wav");
+                        audioFileReader = new AudioFileReader(audioFilePath);
+                        waveOut = new WaveOutEvent();
+                        waveOut.Init(audioFileReader);
+                        waveOut.Play();
 
-                    // 使用 AudioFileReader 來讀取聲音檔
-                    audioFileReader = new AudioFileReader(audioFilePath);
-
-                    // 初始化 WaveOutEvent
-                    waveOut = new WaveOutEvent();
-                    waveOut.Init(audioFileReader);
-
-                    // 播放聲音檔
-                    waveOut.Play();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("無法播放聲音檔，錯誤資訊: " + ex.Message);
-                }
-                finally
-                {
-                    timerAlert.Stop(); // 停止鬧鐘計時器
+                        alarmsRung[i] = true; // 設置該鬧鐘為已響過
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("無法播放聲音檔，錯誤資訊: " + ex.Message);
+                    }
                 }
             }
         }
 
-        // 停止之前的播放
         private void stopWaveOut()
         {
             if (waveOut != null)
@@ -113,18 +99,27 @@ namespace clock
 
         private void btnOn_Click(object sender, EventArgs e)
         {
-            timerAlert.Start(); // 啟動鬧鐘計時器
+            timerAlert.Start();
             btnOn.Enabled = false;
             btnOff.Enabled = true;
-            strSelectTime = cmbHour.SelectedItem.ToString() + ":" + cmbMin.SelectedItem.ToString(); // 擷取小時和分鐘的下拉選單文字，用來設定鬧鐘時間
+
+            strSelectTimes.Clear();
+            alarmsRung = new List<bool> { false, false, false, false, false };
+
+            strSelectTimes.Add(cmbHour.SelectedItem.ToString() + ":" + cmbMin.SelectedItem.ToString());
+            strSelectTimes.Add(cmbHour1.SelectedItem.ToString() + ":" + cmbMin1.SelectedItem.ToString());
+            strSelectTimes.Add(cmbHour2.SelectedItem.ToString() + ":" + cmbMin2.SelectedItem.ToString());
+            strSelectTimes.Add(cmbHour3.SelectedItem.ToString() + ":" + cmbMin3.SelectedItem.ToString());
+            strSelectTimes.Add(cmbHour4.SelectedItem.ToString() + ":" + cmbMin4.SelectedItem.ToString());
         }
 
         private void btnOff_Click(object sender, EventArgs e)
         {
-            stopWaveOut();     // 停止之前的播放
-            timerAlert.Stop(); // 停止鬧鐘計時器
+            stopWaveOut();
+            timerAlert.Stop();
             btnOn.Enabled = true;
             btnOff.Enabled = false;
         }
     }
 }
+
